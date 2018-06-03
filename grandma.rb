1d3dd5e1ee86104b7cc48f1c0c6692e3ea579dde
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-03 01:48:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,9 @@
    
    if phrase != phrase.upcase
      return "HUH?! SPEAK UP, SONNY!"
-   elsif phrase == phrase.upcase
-     return "NO, NOT SINCE 1938!"
    elsif phrase == "I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
+  
      
    end
   end
